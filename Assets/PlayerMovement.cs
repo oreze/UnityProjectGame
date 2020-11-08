191fb7539b9cc8e10ece86523aabf288bb20e7ff
oreze <kunas1@o2.pl>
2020-11-09 00:45:01 +0100

    Add movement script + second free run option handle

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
