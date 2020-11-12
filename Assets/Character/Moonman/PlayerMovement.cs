6f746992199e5c64b9a8ac3deddcaa52ea3375c9
RedRabel99 <lyskopawel03@gmail.com>
2020-11-13 00:09:07 +0100

    Add jump and jump animation. New Camera settings
    Added jump  and jump animation( isnt working properly - to be fixed). Camera now follows our character.

--- Assets/Character/Moonman/PlayerMovement.cs
+++ Assets/Character/Moonman/PlayerMovement.cs
@@ -9,16 +9,26 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
-
+    bool jump = false;
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
