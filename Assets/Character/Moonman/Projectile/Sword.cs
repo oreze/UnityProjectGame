eddd82831e227ca85007438606635d273417658d
oreze <kunas1@o2.pl>
2020-11-23 21:15:01 +0100

    Add projectile collision handling

--- Assets/Character/Moonman/Projectile/Sword.cs
+++ Assets/Character/Moonman/Projectile/Sword.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    public float speed;
+    [Range(0f, 10f)] [SerializeField]public float speed;
     public Rigidbody2D rb;
 
     void Start()
     {
         rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.name.Equals("Player"))
+            Destroy(gameObject, 0.05f);
     }
 }
--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class Projectile : MonoBehaviour
 {
@@ -10,5 +12,6 @@ public class Projectile : MonoBehaviour
    void Start()
     {
         rb.velocity = transform.right * speed;
+        Destroy(rb, 0.5f);
     }
 }
