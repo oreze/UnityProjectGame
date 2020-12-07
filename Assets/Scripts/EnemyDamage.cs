72de4b847c18f2ca738a0de3f110464a3f403752
RedRabel99 <lyskopawel03@gmail.com>
2020-12-07 23:54:32 +0100

    Add Enemy healthbars, one way collision platforms, new platform for merchant, saved all enemies as objects in assets..
    Changes include:
    - Enemy healthbars going from green to red color
    - Every enemy now has his own object, from now on you can edit this asset once, and all of his instances will be changed along with him
    - There is one way platform mechanic. Now you can jump on them from the bottom, and jump off by clicking s. It changes it's collision for all objects, so we need to use platforms wisely(Enemies shouldn't have a chance to stand on them.
    - Merchant now has his own platform to walk on because we can't keep him on one way collision platform

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class EnemyDamage: MonoBehaviour
 {
-    public int health = 150;
+    public int maxHealth = 150;
+    private int health;
     public ParticleSystem bloodSplash;
     private Rigidbody2D rb;
     //public GameObject deathEffect;
+    public HealthbarEnemy healthbar;
 
     void Start()
     {
+        health = maxHealth;
         rb = GetComponent<Rigidbody2D>();
+        if (healthbar) healthbar.setHealth(health, maxHealth);
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
         Instantiate(bloodSplash, new Vector2(rb.position.x, rb.position.y - 0.1f), Quaternion.identity);
+        if (healthbar) healthbar.setHealth(health, maxHealth);
         if (health <= 0)
         {
             Die();
---
+++ Assets/VerticalPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlatform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    void Update()
+    {
+        
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            effector.rotationalOffset = 180f;
+            
+           
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            effector.rotationalOffset = 0;
+        }
+    }
+}
