180e92799ba3e474b7391ea4c43e8f707d1b190d
oreze <kunas1@o2.pl>
2020-12-28 03:39:08 +0100

    - reformat names in PlayerAI

--- Assets/Character/Moonman/PlayerController.cs
+++ Assets/Character/Moonman/PlayerController.cs
@@ -4,107 +4,107 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    public float speed;
-    private float moveInput;
-    public float jumpForce;
-    public Animator animator;
+    private Rigidbody2D RigidBody;
+    public float Speed;
+    private float MoveInput;
+    public float JumpForce;
+    public Animator Animator;
 
-    private bool isGrounded;
-    public Transform feetPos;
-    public float checkRadius;
-    public LayerMask whatIsGround;
-    public Vector2 respawn;
+    private bool IsGrounded;
+    public Transform FeetPosition;
+    public float CheckRadius;
+    public LayerMask WhatIsGround;
+    public Vector2 Respawn;
 
-    private float jumpTimeCounter;
-    public float jumpTime;
-    private bool isJumping;
+    private float JumpTimeCounter;
+    public float JumpTime;
+    private bool IsJumping;
 
-    public int maxHealth = 100;
-    public int currentHealth;
-    public HealthBar healthBar;
-    public ParticleSystem bloodSplash;
+    public readonly int MaxHealth = 100;
+    public int CurrentHealth;
+    public HealthBar HealthBar;
+    public ParticleSystem BloodSplash;
 
-    public Transform firePoint;
-    public GameObject currentWeapon;
-    [Range(0.01f, 10f)] public float attackSpeeed;
-    private float timeBetweenAttacks;
-    private bool canShoot;
+    public Transform FirePoint;
+    public GameObject CurrentWeapon;
+    [Range(0.01f, 10f)] public float AttackSpeed;
+    private float TimeBetweenAttacks;
+    private bool CanShoot;
 
-    private bool m_FacingRight = true;
+    private bool FacingRight = true;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
-        canShoot = true;
-        timeBetweenAttacks = 1.0f / attackSpeeed;
+        RigidBody = GetComponent<Rigidbody2D>();
+        CurrentHealth = MaxHealth;
+        HealthBar.SetMaxHealth(MaxHealth);
+        CanShoot = true;
+        TimeBetweenAttacks = 1.0f / AttackSpeed;
     }
 
     void FixedUpdate()
     {
-        moveInput = Input.GetAxis("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(moveInput));
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-        if (moveInput > 0 && !m_FacingRight)
+        MoveInput = Input.GetAxis("Horizontal");
+        Animator.SetFloat("Speed", Mathf.Abs(MoveInput));
+        RigidBody.velocity = new Vector2(MoveInput * Speed, RigidBody.velocity.y);
+
+        if (MoveInput > 0 && !FacingRight)
         {
             // ... flip the player.
             Flip();
         }
         // Otherwise if the input is moving the player left and the player is facing right...
-        else if (moveInput < 0 && m_FacingRight)
+        else if (MoveInput < 0 && FacingRight)
         {
             // ... flip the player.
             Flip();
         }
 
         if (gameObject.transform.position.y < -50f)
-            gameObject.transform.position = respawn;
+            gameObject.transform.position = Respawn;
 
 
     }
     void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        IsGrounded = Physics2D.OverlapCircle(FeetPosition.position, CheckRadius, WhatIsGround);
+        if (IsGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
-            isJumping = true;
-            jumpTimeCounter = jumpTime;
-            rb.velocity = Vector2.up * jumpForce;
+            IsJumping = true;
+            JumpTimeCounter = JumpTime;
+            RigidBody.velocity = Vector2.up * JumpForce;
         }
-        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        if (Input.GetKey(KeyCode.Space) && IsJumping == true)
         {
-            if (jumpTimeCounter > 0)
+            if (JumpTimeCounter > 0)
             {
-                rb.velocity = Vector2.up * jumpForce;
-                jumpTimeCounter -= Time.deltaTime;
+                RigidBody.velocity = Vector2.up * JumpForce;
+                JumpTimeCounter -= Time.deltaTime;
             }
             else
             {
-                isJumping = false;
+                IsJumping = false;
             }
 
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            isJumping = false;
+            IsJumping = false;
         }
-        if (isGrounded)
+        if (IsGrounded)
         {
-            animator.SetBool("isGrounded", true);
+            Animator.SetBool("IsGrounded", true);
         }
         else
         {
-            animator.SetBool("isGrounded", false);
+            Animator.SetBool("IsGrounded", false);
         }
-        if (Input.GetKeyDown(KeyCode.Return) && canShoot)
+        if (Input.GetKeyDown(KeyCode.Return) && CanShoot)
         {
             Attack();
-            canShoot = false;
-            StartCoroutine(ShootDelay(timeBetweenAttacks));
-            Debug.Log(timeBetweenAttacks);
+            CanShoot = false;
+            StartCoroutine(ShootDelay(TimeBetweenAttacks));
 
         }
 
@@ -114,7 +114,7 @@ public class PlayerController : MonoBehaviour
     private void Flip()
     {
         // Switch the way the player is labelled as facing.
-        m_FacingRight = !m_FacingRight;
+        FacingRight = !FacingRight;
 
         // Multiply the player's x local scale by -1.
         transform.Rotate(0f, 180f, 0f);
@@ -122,26 +122,25 @@ public class PlayerController : MonoBehaviour
 
     void Attack()
     {
-        animator.SetTrigger("Shoot");
-        Instantiate(currentWeapon, firePoint.position, firePoint.rotation);
+        Animator.SetTrigger("Shoot");
+        Instantiate(CurrentWeapon, FirePoint.position, FirePoint.rotation);
 
     }
 
     public void TakeDamage(int damage)
     {
-        Debug.Log(currentHealth + " = CURRENTHEALTH " + damage + " = DAMAGE");
-        currentHealth -= damage;
-        Debug.Log(currentHealth + " = CURRENTHEALTH AFTER ATTACK - DAMAGE " + damage);
-        healthBar.SetHealth(currentHealth);
-        Instantiate(bloodSplash, new Vector3(rb.position.x, rb.position.y - 0.1f, 0), Quaternion.identity);
+        Debug.Log("INFO: " + CurrentHealth + " = CURRENTHEALTH " + damage + " = DAMAGE");
+        CurrentHealth -= damage;
+        HealthBar.SetHealth(CurrentHealth);
+        Instantiate(BloodSplash, new Vector3(RigidBody.position.x, RigidBody.position.y - 0.1f, 0), Quaternion.identity);
     }
 
-    public void CheckForSpecialAttacks(string specialAttack, Transform collision)
+    public void CheckForSpecialAttacks(string name, Transform collision)
     {
-        Debug.Log(specialAttack);
-        if (specialAttack.Equals("SpiritBoxer1"))
+        Debug.Log("INFO: Special attack named " + name + " made on " + collision.gameObject.name);
+        if (name.Equals("SpiritBoxer1"))
         {
-            rb.AddForce(new Vector2(500f * (transform.position.x < collision.transform.position.x ? -1 : 1), 150f));
+            RigidBody.AddForce(new Vector2(500f * (transform.position.x < collision.transform.position.x ? -1 : 1), 150f));
         }
     }
 
@@ -160,7 +159,7 @@ public class PlayerController : MonoBehaviour
     public IEnumerator ShootDelay(float time)
     {
         yield return new WaitForSeconds(time);
-        canShoot = true;
+        CanShoot = true;
     }
 
     public Vector3 CurrentPosition()
