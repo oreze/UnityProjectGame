958b1999fe94cf4591849b2056dabe21657b57e1
RedRabel99 <lyskopawel03@gmail.com>
2021-01-07 23:04:55 +0100

    Created LevelPrefab3, updated some prefabs.
    -Created LevelPrefab3
    -Generator now draws from 3 elements
    - Fixed jumping down from platforms. (Player no longers falls from multiple platforms if he pressed down button once)
    - Applied enemies changes made in FirstLevel scene.
    - Created LevelTestingScene wchich is used to test levels while they are being created.

--- Assets/Scripts/Environment/VerticalPlatform.cs
+++ Assets/Scripts/Environment/VerticalPlatform.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class VerticalPlatform : MonoBehaviour
 {
     private PlatformEffector2D effector;
+    private int platformTimer;
 
     void Start()
     {
         effector = GetComponent<PlatformEffector2D>();
+        platformTimer = 0;
     }
 
     void Update()
@@ -18,10 +20,21 @@ public class VerticalPlatform : MonoBehaviour
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             effector.rotationalOffset = 180f;
+            platformTimer = 2;
         }
-        if (Input.GetKey(KeyCode.Space))
+        /*
+          if (Input.GetKey(KeyCode.Space))
+          {
+              effector.rotationalOffset = 0;
+              platformTimer = 0;
+          }
+          */
+
+        if (platformTimer != 0)
         {
-            effector.rotationalOffset = 0;
+            platformTimer -= 1;
         }
+        else effector.rotationalOffset = 0;
+      
     }
 }
