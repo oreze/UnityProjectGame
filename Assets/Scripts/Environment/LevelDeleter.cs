502eec2296f653def2d6afdb42ac805d5425a062
RedRabel99 <lyskopawel03@gmail.com>
2021-01-11 14:37:46 +0100

    Added LevelDeleter script
    -Added script that deletes levels which are far in the back of the player.

---
+++ Assets/Scripts/Environment/LevelDeleter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDeleter : MonoBehaviour
+{
+    private const float PLAYER_DISTANCE_DELETE_LEVEL_PART = 10f;
+
+    protected Transform Target;
+    private Vector3 EndPosition;
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        EndPosition = gameObject.transform.Find("EndPosition").position;
+    }
+
+    void Update()
+    {
+        if (!Target)
+        {
+            Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        }
+
+        if(EndPosition.x < Target.position.x)
+        {
+            if (Vector3.Distance(Target.position, EndPosition) > PLAYER_DISTANCE_DELETE_LEVEL_PART)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
