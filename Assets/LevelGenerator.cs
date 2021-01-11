8524c682dd805272b575b43e82a79d424275270b
oreze <kunas1@o2.pl>
2021-01-11 16:39:19 +0100

    Add enemy spawners at each level prefab
    TODO:
    - randomize enemy to spawn
    - add spawning points to every level prefab

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class LevelGenerator : MonoBehaviour
 {
---
+++ Assets/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> Enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<GameObject> SpawnPoints = FindObjectsOfTypeAll(typeof(GameObject)).Cast<GameObject>().Where(g => g.tag == "SpawnPoint").ToList();
+
+        foreach(GameObject point in SpawnPoints)
+        {
+            Instantiate(Enemies[0], point.transform.position + transform.position, Quaternion.identity);
+        }
+    }
+}
