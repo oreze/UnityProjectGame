881c5244221bbaaa40645919171d4131e21638a9
RedRabel99 <lyskopawel03@gmail.com>
2021-01-15 22:37:32 +0100

    Menu Update
    -Created new main menu
    - Start button starts a game, options button opens options, which have volume slider and reset score button.

--- Assets/Scenes/GameMenu.cs
+++ Assets/Scenes/GameMenu.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
+    public Text HighScore, HighScore2;
+    private bool WasDeleted;
+
+    public void Start()
+    {
+        HighScore.text = "HIGH SCORE\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+        HighScore2.text = "HIGH SCORE\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+        WasDeleted = false;
+    }
+
+    public void Update()
+    {
+        if (WasDeleted)
+        {
+            HighScore.text = "HIGH SCORE\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+            HighScore2.text = "HIGH SCORE\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+        }  
+    }
     public void PlayFreeRun()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
     public void QuitMenu()
     {
         UnityEngine.Debug.Log("QUIT");
         Application.Quit();
+    }
+
+    public void ResetScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+        WasDeleted = true;
+
     }
 }
