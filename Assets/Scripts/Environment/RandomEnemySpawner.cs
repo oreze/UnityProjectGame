3bf7d6cd5420674a18d240024fb820c5ac335256
RedRabel99 <lyskopawel03@gmail.com>
2021-01-21 15:01:23 +0100

    RandomEnemy spawner Fix

--- Assets/Scripts/Environment/RandomEnemySpawner.cs
+++ Assets/Scripts/Environment/RandomEnemySpawner.cs
@@ -15,7 +15,7 @@ public class RandomEnemySpawner : MonoBehaviour
         totalChance = enemies.Count * 100;
         int chance = Random.Range(0, totalChance);
         //Debug.Log(name + " " + (chances[chance / 100] + (100 * (chance / 100))) + " " + chance);
-        if (chances[chance / 100]+ (100*(chance/100)) <= chance) {
+        if (chances[chance / 100]+ (100*(chance/100)) >= chance) {
            // Debug.Log("Spawn " + enemies[chance / 100].name);
             Enemy = Instantiate(enemies[chance / 100], gameObject.transform.position, Quaternion.identity);
             Enemy.transform.parent = Parent.transform;
