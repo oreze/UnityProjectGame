ce8c252815a262e4d369648841719a81d4c07105
RedRabel99 <lyskopawel03@gmail.com>
2021-01-21 20:03:29 +0100

    Created tutorial level as first level, added volume handler

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        AudioListener.volume = PlayerPrefs.GetFloat("Volume", 0.5f);
+    }
+}
--- Assets/Scenes/GameMenu.cs
+++ Assets/Scenes/GameMenu.cs
@@ -7,8 +7,9 @@ using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
-    public Text HighScore, HighScore2;
-    private bool WasDeleted;
+    public Text HighScore, HighScore2, VolumeValue;
+    private bool WasDeleted, VolumeWasChanged;
+   
 
     public void Start()
     {
@@ -19,11 +20,19 @@ public class GameMenu : MonoBehaviour
 
     public void Update()
     {
+        VolumeWasChanged = true;
         if (WasDeleted)
         {
             HighScore.text = "HIGH SCORE\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
             HighScore2.text = "HIGH SCORE\n" + PlayerPrefs.GetInt("HighScore", 0).ToString();
-        }  
+        }
+        VolumeValue.text = (PlayerPrefs.GetFloat("Volume", 0.5f) * 100).ToString("0");
+
+        if (VolumeWasChanged)
+        {
+            VolumeWasChanged = false;
+            AudioListener.volume = PlayerPrefs.GetFloat("Volume", 0.5f)/2f;
+        }
     }
     public void PlayFreeRun()
     {
@@ -40,5 +49,19 @@ public class GameMenu : MonoBehaviour
         PlayerPrefs.DeleteKey("HighScore");
         WasDeleted = true;
 
+    }
+
+    public void TurnUpVolume()
+    {
+        float volume = PlayerPrefs.GetFloat("Volume", 0.5f);
+        if (volume < 1) PlayerPrefs.SetFloat("Volume", volume + 0.05f);
+        VolumeWasChanged = true;
+    }
+    
+    public void LowerDownVolume()
+    {
+        float volume = PlayerPrefs.GetFloat("Volume", 0.5f);
+        if (volume > 0.04f) PlayerPrefs.SetFloat("Volume", volume - 0.05f);
+        VolumeWasChanged = true;
     }
 }
